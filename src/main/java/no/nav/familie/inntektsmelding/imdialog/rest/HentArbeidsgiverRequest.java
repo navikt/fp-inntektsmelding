a36535179e4a3d1f44ca5e191befaf48faf22c30
anjaaalerud <anja.gotesen.aalerud@nav.no>
2025-04-25 13:50:11 +0200

    POFIM-221 fjerner utilsiktet endring

--- src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/HentArbeidsgiverRequest.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/HentArbeidsgiverRequest.java
@@ -9,6 +9,6 @@ import no.nav.familie.inntektsmelding.integrasjoner.person.PersonIdent;
 import no.nav.familie.inntektsmelding.koder.Ytelsetype;
 
 public record HentArbeidsgiverRequest(@Valid @NotNull PersonIdent fødselsnummer,
-                                         @Valid @NotNull Ytelsetype ytelseType,
-                                     @Valid @NotNull LocalDate førsteFraværsdag) {
+                                      @Valid @NotNull Ytelsetype ytelseType,
+                                      @Valid @NotNull LocalDate førsteFraværsdag) {
 }
