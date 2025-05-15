fefa3846de27eb2ab375a2b199d1169e7120e285
anjaaalerud <anja.gotesen.aalerud@nav.no>
2025-05-15 13:36:47 +0200

    POFIM-221 fjerner ubrukt import

--- src/main/java/no/nav/familie/inntektsmelding/integrasjoner/fpsak/FpsakTjeneste.java
+++ src/main/java/no/nav/familie/inntektsmelding/integrasjoner/fpsak/FpsakTjeneste.java
@@ -3,7 +3,6 @@ package no.nav.familie.inntektsmelding.integrasjoner.fpsak;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import no.nav.familie.inntektsmelding.integrasjoner.person.PersonInfo;
 import no.nav.familie.inntektsmelding.koder.Ytelsetype;
 import no.nav.familie.inntektsmelding.typer.entitet.AktørIdEntitet;
 
