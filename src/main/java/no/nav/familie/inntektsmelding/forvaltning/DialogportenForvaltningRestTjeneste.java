42dcc3d32300e2519e346e47495fcc529a91128e
Michal J. Sladek <michal.sladek@nav.no>
2025-06-24 15:14:27 +0200

    Fikser validering

--- src/main/java/no/nav/familie/inntektsmelding/forvaltning/DialogportenForvaltningRestTjeneste.java
+++ src/main/java/no/nav/familie/inntektsmelding/forvaltning/DialogportenForvaltningRestTjeneste.java
@@ -2,20 +2,24 @@ package no.nav.familie.inntektsmelding.forvaltning;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import jakarta.ws.rs.core.Response;
 import no.nav.familie.inntektsmelding.integrasjoner.altinn.AltinnDialogportenKlient;
 import no.nav.familie.inntektsmelding.server.auth.api.AutentisertMedAzure;
 import no.nav.familie.inntektsmelding.server.auth.api.Tilgangskontrollert;
 import no.nav.familie.inntektsmelding.server.tilgangsstyring.Tilgang;
+import no.nav.familie.inntektsmelding.typer.dto.OrganisasjonsnummerDto;
 import no.nav.foreldrepenger.konfig.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
 
 @ApplicationScoped
 @Path(DialogportenForvaltningRestTjeneste.BASE_PATH)
@@ -42,16 +46,19 @@ public class DialogportenForvaltningRestTjeneste {
     @Path("/opprettDialog")
     @Consumes(MediaType.APPLICATION_JSON)
     @Tilgangskontrollert
-    public String opprettDialog(String forespørselUuid, String organisasjonsnummer) {
+    public Response opprettDialog(@NotNull @Valid OpprettNyDialogDto opprettNyDialogDto) {
         if (IS_PROD) {
             throw new IllegalStateException("Kan ikke opprette dialog i produksjon. Bruk testmiljø for dette.");
         }
         sjekkAtKallerHarRollenDrift();
-        LOG.info("Oppretter en dialog for foresprørselUuid {} og organisasjonsnummer {}", forespørselUuid, organisasjonsnummer);
-        return altinnDialogportenKlient.opprettDialog(forespørselUuid, organisasjonsnummer);
+        LOG.info("Oppretter en dialog for foresprørselUuid {} og organisasjonsnummer {}", opprettNyDialogDto.forespørselUuid(), opprettNyDialogDto.organisasjonsnummer().orgnr());
+        return Response.accepted(altinnDialogportenKlient.opprettDialog(opprettNyDialogDto.organisasjonsnummer().orgnr(), opprettNyDialogDto.forespørselUuid().toString())).build();
     }
 
     private void sjekkAtKallerHarRollenDrift() {
         tilgang.sjekkAtAnsattHarRollenDrift();
+    }
+
+    public record OpprettNyDialogDto(@NotNull @Valid UUID forespørselUuid, @NotNull @Valid OrganisasjonsnummerDto organisasjonsnummer) {
     }
 }
