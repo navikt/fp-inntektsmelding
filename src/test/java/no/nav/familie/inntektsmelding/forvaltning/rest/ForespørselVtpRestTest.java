ebf4c9e5222f7658bea1dea9161234ad412b8b20
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-10-21 15:11:08 +0200

    Siste bom

--- src/test/java/no/nav/familie/inntektsmelding/forvaltning/rest/ForespørselVtpRestTest.java
+++ src/test/java/no/nav/familie/inntektsmelding/forvaltning/rest/ForespørselVtpRestTest.java
@@ -1,6 +1,6 @@
 package no.nav.familie.inntektsmelding.forvaltning.rest;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
