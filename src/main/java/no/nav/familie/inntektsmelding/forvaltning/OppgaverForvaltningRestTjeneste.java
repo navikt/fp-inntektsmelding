b73be434db6bcd71d51a65314963d0a94a26ffbd
anjaaalerud <anja.gotesen.aalerud@nav.no>
2026-02-18 08:59:06 +0100

    TFP-6614 fikser regex for String for OppgaverForvaltningRestTjeneste for å sette forespørsel til utgått

--- src/main/java/no/nav/familie/inntektsmelding/forvaltning/OppgaverForvaltningRestTjeneste.java
+++ src/main/java/no/nav/familie/inntektsmelding/forvaltning/OppgaverForvaltningRestTjeneste.java
@@ -84,7 +84,7 @@ public class OppgaverForvaltningRestTjeneste {
     @Tilgangskontrollert
     public Response settForespørselOgSakTilUtgått(
         @Parameter(description = "UUID for forespørsel som skal settes til utgått") @Valid @NotNull @PathParam("forespørselUuid")
-        @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", message = "Ugyldig UUID-format")
+        @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", message = "Ugyldig UUID-format")
         String forespørselUuid) {
         var gyldigForespørselUuid = UUID.fromString(forespørselUuid);
         sjekkAtKallerHarRollenDrift();
