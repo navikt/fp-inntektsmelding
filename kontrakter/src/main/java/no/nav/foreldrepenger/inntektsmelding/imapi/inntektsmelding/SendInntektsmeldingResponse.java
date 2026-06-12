b23093dba66c0bd54949806c3d2d2ba497c68d9d
anjaaalerud <anja.gotesen.aalerud@nav.no>
2026-06-12 15:49:48 +0200

    TFP-6982 legger til status på SendInntektsmeldingResponse kontrakt

--- kontrakter/src/main/java/no/nav/foreldrepenger/inntektsmelding/imapi/inntektsmelding/SendInntektsmeldingResponse.java
+++ kontrakter/src/main/java/no/nav/foreldrepenger/inntektsmelding/imapi/inntektsmelding/SendInntektsmeldingResponse.java
@@ -1,11 +1,10 @@
 package no.nav.foreldrepenger.inntektsmelding.imapi.inntektsmelding;
 
 import no.nav.foreldrepenger.inntektsmelding.felles.FeilkodeDto;
+import no.nav.foreldrepenger.inntektsmelding.felles.InntektsmeldingStatusDto;
 
 import java.util.UUID;
 
-// TODO: Legg til InntektsmeldingStatusDto status som parameter nr 3:
-//       (boolean success, UUID inntektsmeldingUuid, InntektsmeldingStatusDto status, FeilInfo feilinformasjon)
-public record SendInntektsmeldingResponse(boolean success, UUID inntektsmeldingUuid, FeilInfo feilinformasjon) {
+public record SendInntektsmeldingResponse(boolean success, UUID inntektsmeldingUuid, InntektsmeldingStatusDto status, FeilInfo feilinformasjon) {
     public record FeilInfo(FeilkodeDto feilkode, String feilmelding, String referanseId) {}
 }
