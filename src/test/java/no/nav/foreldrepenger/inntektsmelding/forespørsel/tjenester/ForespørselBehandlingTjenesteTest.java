fb423241c9de5d84bd946e83e2209a6f71f3e19c
anjaaalerud <anja.gotesen.aalerud@nav.no>
2026-08-06 14:33:58 +0200

    TFP-7099 fjerner ubrukt import

--- src/test/java/no/nav/foreldrepenger/inntektsmelding/forespørsel/tjenester/ForespørselBehandlingTjenesteTest.java
+++ src/test/java/no/nav/foreldrepenger/inntektsmelding/forespørsel/tjenester/ForespørselBehandlingTjenesteTest.java
@@ -2,7 +2,6 @@ package no.nav.foreldrepenger.inntektsmelding.forespørsel.tjenester;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
 
