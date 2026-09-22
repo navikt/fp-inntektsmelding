f2c074db58d61df1ab5f89d36b17401dddf0bee4
anjaaalerud <anja.gotesen.aalerud@nav.no>
2026-09-22 15:03:38 +0200

    TFP-6988 Kontroll av inntekt ved innsending fra dialog
    * gjør ikke sjekk mot inntekt om årsak er oppgitt ved innsending av im fra front end

--- src/main/java/no/nav/foreldrepenger/inntektsmelding/imdialog/tjenester/InntektsmeldingMottakTjeneste.java
+++ src/main/java/no/nav/foreldrepenger/inntektsmelding/imdialog/tjenester/InntektsmeldingMottakTjeneste.java
@@ -59,20 +59,25 @@ public class InntektsmeldingMottakTjeneste {
 
         fellesMottakTjeneste.settForrigeInntektsmeldingUtdatertHvisVenterVurdering(forespørsel);
 
-        var kontrollResultat = inntektKontrollTjeneste.sjekkInntektMotAInntekt(forespørsel, mottattInntektsmeldingDto);
+        //Vi trenger ikke å sjekke inntekt om årsak allerede er oppgitt
+        if (mottattInntektsmeldingDto.getEndringAvInntektÅrsaker().isEmpty()) {
+            var kontrollResultat = inntektKontrollTjeneste.sjekkInntektMotAInntekt(forespørsel, mottattInntektsmeldingDto);
 
-        if (kontrollResultat instanceof InntektKontrollResultat.UlikInntekt(_, var inntektFraAInntekt)) {
-            throw new InntektAvvikerFraAInntektException(inntektFraAInntekt.gjennomsnitt(), mottattInntektsmeldingDto.getMånedInntekt());
-        }
+            if (kontrollResultat instanceof InntektKontrollResultat.UlikInntekt(_, var inntektFraAInntekt)) {
+                throw new InntektAvvikerFraAInntektException(inntektFraAInntekt.gjennomsnitt(), mottattInntektsmeldingDto.getMånedInntekt());
+            }
 
-        if (kontrollResultat instanceof InntektKontrollResultat.Nedetid) {
-            // A-inntekt har nedetid - lagrer inntektsmeldingen med status VENTER_VURDERING og etterkontrollerer
-            // asynkront (se InntektKontrollTjeneste.kontrollerInntektsmeldingEtterNedetid). Forespørselen
-            // ferdigstilles ikke, og portalene varsles først når resultatet av etterkontrollen foreligger.
-            var inntektsmeldingMedStatus = InntektsmeldingDto.builder(mottattInntektsmeldingDto).medStatus(InntektsmeldingStatus.VENTER_VURDERING).build();
-            var lagretIm = fellesMottakTjeneste.lagreImOgOpprettTaskForEtterkontroll(inntektsmeldingMedStatus, forespørsel);
-            MetrikkerTjeneste.loggInnsendtInntektsmeldingUnderNedetid();
-            return InntektsmeldingMapper.mapFraDomene(lagretIm, forespørsel);
+            if (kontrollResultat instanceof InntektKontrollResultat.Nedetid) {
+                // A-inntekt har nedetid - lagrer inntektsmeldingen med status VENTER_VURDERING og etterkontrollerer
+                // asynkront (se InntektKontrollTjeneste.kontrollerInntektsmeldingEtterNedetid). Forespørselen
+                // ferdigstilles ikke, og portalene varsles først når resultatet av etterkontrollen foreligger.
+                var inntektsmeldingMedStatus = InntektsmeldingDto.builder(mottattInntektsmeldingDto)
+                    .medStatus(InntektsmeldingStatus.VENTER_VURDERING)
+                    .build();
+                var lagretIm = fellesMottakTjeneste.lagreImOgOpprettTaskForEtterkontroll(inntektsmeldingMedStatus, forespørsel);
+                MetrikkerTjeneste.loggInnsendtInntektsmeldingUnderNedetid();
+                return InntektsmeldingMapper.mapFraDomene(lagretIm, forespørsel);
+            }
         }
 
         var lagretIm = fellesMottakTjeneste.lagreImOgOpprettJournalførTask(mottattInntektsmeldingDto, forespørsel);
